5f5f3e69ff03f5e80e1eb031fa2d1a7599e17f20
10o0o <wotjs2923@gmail.com>
2021-07-01 17:32:49 +0900

    'routerfin'

--- src/index.js
+++ src/index.js
@@ -3,47 +3,10 @@ const Router = require('koa-router');
 
 const app = new koa();
 const router = new Router();
+const api = require('./api');
 
-// app.use(async (ctx, next) => {
-//   // console.log(1);
-//   // const started = new Date();
-//   // next().then(() => {
-//   //   console.log(new Date() - started + 'ms');
-//   // })
-  
-//   console.log(1);
-//   const started = new Date();
-//   await next();
-//   console.log(new Date() - started + 'ms');
-// });
 
-// app.use((ctx, next) => {
-//   console.log(2);
-//   next();
-// })
-
-// app.use(ctx => {
-//   ctx.body = 'Hello Koa';
-// });
-
-router.get('/', (ctx, next) => {
-  ctx.body = 'home';
-});
-
-router.get('/test', (ctx, next) => {
-  ctx.body = 'test';
-});
-
-router.get('/about/:name', (ctx, next) => {
-  const { name } = ctx.params;
-  ctx.body = name + '의 소개';
-})
-
-router.get('/post', (ctx, next) => {
-  const { id } = ctx.request.query;
-  if(id) ctx.body = '포스트 #' + id;
-  else ctx.body = '포스트 아이디가 없습니다.';
-});
+router.use('/api', api.routes())
 
 app.use(router.routes());
 app.use(router.allowedMethods());
