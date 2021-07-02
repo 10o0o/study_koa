efe3e3920a810b3f4a544bad1d35f5682078b408
10o0o <wotjs2923@gmail.com>
2021-07-02 10:49:52 +0900

    get, post method complete

--- src/api/books/books.controller.js
+++ src/api/books/books.controller.js
@@ -1,13 +1,115 @@
-exports.list = (ctx) => {
-  ctx.body = 'listed';
+const Book = require('models/book')
+
+exports.get = async (ctx) => {
+  const { id } = ctx.params;
+
+  let book;
+
+  try {
+    book = await Book.findById(id).exec();
+  } catch (e) {
+    if (e.name === 'CastError') {
+      ctx.status = 400;
+      ctx.body = {
+        message: '잘못된 id값입니다.',
+        data: null
+      }
+      return;
+    }
+    return ctx.throw(500, e);
+  }
+
+  if (!book) {
+    ctx.status = 404;
+    ctx.body = { message: 'book is not found' };
+    return;
+  }
+
+  ctx.body = book;
+}
+
+exports.list = async (ctx) => {
+  let books, count, isDesc;
+
+  if (ctx.query) {
+    count = ctx.query.count;
+    isDesc = ctx.query.isDesc;
+  }
+
+  try {
+    if (count && isDesc === 'Desc') {
+      console.log('first');
+      books = await Book.find().exeC()
+      .sort({ _id: -1 })
+      .limit(Number(count))
+      .exec();
+    } else if (count) {
+      console.log('second');
+      books = await Book.find().exec()
+      .limit(Number(count))
+      .exec();
+    } else if (isDesc === 'Desc') {
+      console.log('third');
+      books = await Book.find().exec()
+      .sort({ _id: -1 })
+      .exec();
+    } else {
+      console.log('fourth');
+      books = await Book.find().exec();
+    }
+  } catch (e) {
+    return ctx.throw(500, e);
+  }
+
+  ctx.body = books;
 };
 
-exports.create = (ctx) => {
-  ctx.body = 'created';
+exports.create = async (ctx) => {
+  const { title, authors, publishedDate, price, tags } = ctx.request.body;
+
+  const book = new Book({ title, authors, publishedDate, price, tags });
+
+  try {
+    await book.save().exeC();
+  } catch(e) {
+    return ctx.throw(500, e);
+  }
+
+  ctx.body = book;
 };
 
-exports.delete = (ctx) => {
-  ctx.body = 'deleted';
+exports.delete = async (ctx) => {
+  const { id } = ctx.params;
+  let book;
+
+  try {
+    book = await Book.findByIdAndRemove(id).exec();
+  } catch(e) {
+    if (e.name === 'CastError') {
+      console.log('cast error occur')
+      ctx.status = 400;
+      ctx.body = {
+        message: '유효하지 않는 ID',
+        data: null,
+      }
+      return
+    }
+  }
+
+  if (!book) {
+    ctx.status = 404;
+    ctx.body = {
+      message: '이미 삭제되었거나 없는 책입니다.',
+      data: null,
+    }
+    return;
+  }
+
+  // ctx.status = 204; // No Content
+  ctx.body = {
+    message: 'SuccessFully Deleted',
+    data: book
+  }
 };
 
 exports.replace = (ctx) => {
--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const koa = require('koa');
 const Router = require('koa-router');
 
@@ -5,12 +7,30 @@ const app = new koa();
 const router = new Router();
 const api = require('./api');
 
+const mongoose = require('mongoose');
+const bodyParser = require('koa-bodyparser')
+
+// ?
+mongoose.Promise = global.Promise;
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(
+  (response) => {
+    console.log('Successfully connected to mongodb');
+  }
+).catch(e => {
+  console.error(e);
+});
+
+const port = process.env.PORT || 4000;
+
+app.use(bodyParser());
 
 router.use('/api', api.routes())
+app.use(router.routes()).use(router.allowedMethods());
 
-app.use(router.routes());
-app.use(router.allowedMethods());
-
-app.listen(4000, () => {
+app.listen(port, () => {
   console.log('herum server is listening to port 4000');
 });
